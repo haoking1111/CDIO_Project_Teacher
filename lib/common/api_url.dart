a8d1c9cbf1f757cfaa9b1e008a6a448d5483f8ad
haoking1111 <ngbao08052003@gmail.com>
2024-05-12 16:39:20 +0700

    commit

--- lib/common/api_url.dart
+++ lib/common/api_url.dart
@@ -1,5 +1,5 @@
 class ApiUrl {
-  static const apiUrl ='http://192.168.50.210:8080';
+  static const apiUrl ='http://192.168.1.10:8080';
   static const authUrl = '$apiUrl/api/auth/login';
   static const getParentUrl = '$apiUrl/api/user';
   static const getChildUrl = '$apiUrl/api/children/parent';
