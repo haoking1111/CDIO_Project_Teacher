717820859b5d126f3133ca4a8a2ab7ab53129381
ngb4o <ngb08052003@gmail.com>
2024-05-25 23:21:07 +0700

    api commit

--- lib/common/api_url.dart
+++ lib/common/api_url.dart
@@ -1,5 +1,5 @@
 class ApiUrl {
-  static const apiUrl ='http://192.168.1.6:8080';
+  static const apiUrl ='http://152.42.238.98:8080';
   static const authUrl = '$apiUrl/api/auth/login';
   static const getParentUrl = '$apiUrl/api/user';
   static const getChildUrl = '$apiUrl/api/children/parent';
